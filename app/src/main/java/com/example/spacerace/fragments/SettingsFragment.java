a7845fb61e5c71f67d491dd51e6c428f341d3c5f
Zachary Allard <zackallard17@gmail.com>
2021-03-25 11:22:09 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/spacerace/fragments/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.example.spacerace.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.spacerace.R;
+
+public class SettingsFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/spacerace/fragments/WordFragment.java
@@ -0,0 +1,25 @@
+package com.example.spacerace.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.spacerace.R;
+
+public class WordFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_word, container, false);
+
+        return view;
+    }
+}
